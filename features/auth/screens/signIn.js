dec99ca0bdd47583fd13ceb59e7bd8e8b65a1fd9
Saranyapong Phoksawas <sranyapong47@gmail.com>
2025-10-07 14:45:23 +0700

    Signin

--- features/auth/screens/signIn.js
+++ features/auth/screens/signIn.js
@@ -2,11 +2,11 @@ import React from "react";
 import { View, Text ,Image} from "react-native";
 
 export const SignIn = () => {
-    const uri = { require('../../../assets/Logo/deli.svg')}
+    const uri = require('../../../assets/Logo/deli.svg');
   return (
     <View style={{ flex: 1}}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" ,backgroundColor:'black' ,opacity:0.65}}>
-        <Image source ={require(uri)} style={{width:100 , height:100}}></Image>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Image source ={uri} style={{width:100 , height:100}} />
       </View>
       <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
         <Text>SignIn</Text>
